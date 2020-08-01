9b7b9131c947309cb567ba747863cc790643a76d
evgenytokmakov <evgenytokmakov@gmail.com>
2020-08-02 00:40:54 +0300

    Astronomy

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -2,4 +2,34 @@ package ru.progwards.java1.lessons.basics;
 
 public class Astronomy {
 
+    public static Double sphereSquare(Double r){
+    double S=4*3.14*r*r;
+    System.out.println(S);
+    return(S);
+    }
+
+    public static Double earthSquare(){
+        return(sphereSquare(6371.2));
+    }
+
+    public static Double mercurySquare(){
+        return(sphereSquare(2439.7));
+    }
+
+    public static Double jupiterSquare(){
+        return(sphereSquare(71492.0));
+    }
+
+    public static Double earthVsMercury(){
+        return(earthSquare()/mercurySquare());
+    }
+
+    public static Double earthVsJupiter(){
+        return(earthSquare()/jupiterSquare());
+    }
+    public static void main(String[] args) {
+        earthSquare();
+        mercurySquare();
+        jupiterSquare();
+    }
 }
