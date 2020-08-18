af49a9d499bc78d3315458214a106d73854c00bd
evgenytokmakov <evgenytokmakov@gmail.com>
2020-08-18 03:10:15 +0300

    mensh

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -1,4 +1,35 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
+    public static boolean isTriangle(int a, int b, int c){
+        boolean otvet;
+        boolean SummaBCkA=false;
+        boolean SummaACkB=false;
+        boolean SummaABkC=false;
+        if(a<(b+c)){
+            SummaBCkA=true;
+        }
+        if(b<(a+c)){
+            SummaACkB=true;
+        }
+        if(c<(a+b)){
+            SummaABkC=true;
+        }
+        if(SummaBCkA&SummaACkB&SummaABkC==true){
+            otvet=true;
+            System.out.println("Можно)");
+            return otvet;
+
+        }
+        else {
+            otvet=false;
+            System.out.println("НЕ Можно)");
+            return otvet;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        isTriangle(2,25,11);
+    }
 }
